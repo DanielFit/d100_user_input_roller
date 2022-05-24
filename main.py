f33776388367b22ceeb24ec2af61d8ac68756491
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-25 09:00:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from skills import *
 import char2 as char
 
-"""When making input choose form a list of skills as listed on the screen below. Please
-ensure any additional keywords are entered along with your skill. 
+"""
+When entering an input choose fomr the list of skills as seen on the input screen below. Please
+ensure any additional keywords are entered along with your skill ie: "insight adv" instead of across multiple inputs. 
 
-If advantage or disadvantage is called by the GM simply type "adv" or "disadv" respectively
-on the same line you enter your skill.
+If advantage or disadvantage is required simply type "adv" or "disadv".
 
- If a penalty is called by the GM simply type "easy" "medium" or "hard" following your skill
- on the same line"""
+ If a penalty is required type "easy" "medium" or "hard" 
+ """
 
 
 
