d3cc6d11464226908d0efff6965f7df67ca462ae
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-25 09:13:03 +1000

    Add files via upload

---
+++ advantage_diceroller.py
@@ -0,0 +1,18 @@
+"""
+This functions purpose is provide the lower or higher number of two dice rolls, 
+with advantage providing the lower number and disadvantage providing the higher number.
+"""
+
+from random import randint
+
+
+def rolltwodie(x):
+    doubleroll = [randint(1, 100), randint(1, 100)]
+    adv = min(doubleroll)
+    disadv = max(doubleroll)
+    if x == "adv":
+     return adv
+    if x == "disadv":
+     return disadv
+    if x != adv or disadv:
+        print(" try adv or disadv")
