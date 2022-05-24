a9dd30aff2735c7139f25bb92332d22e3a134601
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-25 09:01:28 +1000

    Update char1.py

--- char1.py
+++ char1.py
@@ -1,2 +1,6 @@
+"""
+This is a sample character sheet containing the two elemtns required for this game , a dicitonairy of the charcters skills and their maximum health for the game. 
+"""
+
 skills = {"strength":70,"speed":60,"iq":50,"charm":80, "insight": 45}
 health = 2
