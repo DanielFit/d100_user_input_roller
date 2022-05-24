3cfc6743e605b03fa143bf0d052c34181e120d27
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-24 11:02:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from skills import *
 import char2 as char
 
+"""When making input choose form a list of skills as listed on the screen below. Please
+ensure any additional keywords are entered along with your skill. 
+
+If advantage or disadvantage is called by the GM simply type "adv" or "disadv" respectively
+on the same line you enter your skill.
+
+ If a penalty is called by the GM simply type "easy" "medium" or "hard" following your skill
+ on the same line"""
+
+
+
 skills = char.skills
 health = char.health
 
